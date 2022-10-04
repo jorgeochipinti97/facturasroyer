554f1f5b2ec716ec1718818621d78fa5cd9963f8
Jorge <jorge@Air-de-Jorge.fibertel.com.ar>
2022-10-04 11:12:33 -0300

    s

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Chip, Divider, TextField, Typography } from '@mui/material';
 import type { NextPage } from 'next'
+import Head from 'next/head';
 import { useState } from 'react';
 import { InvoicePage } from '../components/Invoice/index';
 import { _InterfaceInvoice } from '../interface';
@@ -36,123 +37,126 @@ const Home: NextPage = () => {
   }
 
   return (
+    <>
+      <Head>
+        <title>Royer-Store | Factura</title>
+      </Head>
+      <Box>
+        <Box sx={{ m: 2, }} display='flex' justifyContent='center'>
+          <Box sx={{ m: 2, borderRadius: '10px', border: '1px solid black', width: 500 }}>
+            <Typography variant='h3' sx={{ textAlign: 'center', m: 4 }}>Datos de la Factura</Typography>
 
-    <Box>
+            <Box display='flex' justifyContent='center'>
+              <Box display='flex' flexDirection='column' justifyContent='space-around'>
+                <Box>
+                  <TextField label={'Nombre completo'}
+                    sx={{ m: 1 }}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </Box>
+                <Box>
+                  <TextField label='dirección'
+                    sx={{ m: 1 }}
+                    onChange={(e) => setAddress(e.target.value)}
 
-      <Box sx={{ m: 2, }} display='flex' justifyContent='center'>
-        <Box sx={{ m: 2, borderRadius: '10px', border: '1px solid black', width: 500 }}>
-          <Typography variant='h3' sx={{ textAlign: 'center', m: 4 }}>Datos de la Factura</Typography>
+                  />
+                </Box>
+                <Box>
+                  <TextField label='Método de Pago'
+                    sx={{ m: 1 }}
+                    onChange={(e) => setPayMethod(e.target.value)}
 
-          <Box display='flex' justifyContent='center'>
-            <Box display='flex' flexDirection='column' justifyContent='space-around'>
-              <Box>
-                <TextField label={'Nombre completo'}
-                  sx={{ m: 1 }}
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </Box>
-              <Box>
-                <TextField label='dirección'
-                  sx={{ m: 1 }}
-                  onChange={(e) => setAddress(e.target.value)}
-
-                />
-              </Box>
-              <Box>
-                <TextField label='Método de Pago'
-                  sx={{ m: 1 }}
-                  onChange={(e) => setPayMethod(e.target.value)}
-
-                />
-              </Box>
-              <Box>
-                <TextField label='Numero de Orden'
-                  sx={{ m: 1 }}
-                  onChange={(e) => setNOrder(parseInt(e.target.value))}
-                />
-              </Box>
-              <Box>
-                <TextField label='Total' type='number'
-                  sx={{ m: 1 }}
-                  onChange={(e) => setTotal(parseInt(e.target.value))}
-                />
-              </Box>
-            </Box>
-          </Box>
-        </Box>
-      </Box>
-      <Divider sx={{ backgroundColor: 'black' }} />
-      <Box sx={{ m: 4 }} display='flex' justifyContent='center'>
-        <Box sx={{ border: '1px solid black', width: 500, borderRadius: '10px' }}>
-          <Typography variant='h3' sx={{ textAlign: 'center', mb: 4, }}>Agregar Producto</Typography>
-          <Box display='flex' justifyContent='center'>
-            <Box display='flex' flexDirection='column'>
-              <Box>
-                <TextField label='Nombre'
-
-                  sx={{ m: 1 }}
-                  onChange={(e) => setNameOfProduct(e.target.value)}
-                />
-              </Box>
-              <Box>
-                <TextField label='descripción'
-                  sx={{ m: 1 }}
-                  onChange={(e) => setDescription(e.target.value)}
-                />
-              </Box>
-              <Box>
-                <TextField label='Precio' type='number'
-                  sx={{ m: 1 }}
-                  onChange={(e) => setPrice(parseInt(e.target.value))}
-
-                />
-              </Box>
-              <Box>
-                <TextField label='Cantidad' type='number'
-                  sx={{ m: 1 }}
-                  onChange={(e) => setQuantity(parseInt(e.target.value))}
-
-                />
+                  />
+                </Box>
+                <Box>
+                  <TextField label='Numero de Orden'
+                    sx={{ m: 1 }}
+                    onChange={(e) => setNOrder(parseInt(e.target.value))}
+                  />
+                </Box>
+                <Box>
+                  <TextField label='Total' type='number'
+                    sx={{ m: 1 }}
+                    onChange={(e) => setTotal(parseInt(e.target.value))}
+                  />
+                </Box>
               </Box>
             </Box>
           </Box>
         </Box>
-      </Box>
-      <Box display='flex' justifyContent='center' sx={{ mt: 4 }}>
-        <Button onClick={() => onCreateProduct()} variant='contained'>
-          agregar producto
-        </Button>
-      </Box>
+        <Divider sx={{ backgroundColor: 'black' }} />
+        <Box sx={{ m: 4 }} display='flex' justifyContent='center'>
+          <Box sx={{ border: '1px solid black', width: 500, borderRadius: '10px' }}>
+            <Typography variant='h3' sx={{ textAlign: 'center', mb: 4, }}>Agregar Producto</Typography>
+            <Box display='flex' justifyContent='center'>
+              <Box display='flex' flexDirection='column'>
+                <Box>
+                  <TextField label='Nombre'
 
+                    sx={{ m: 1 }}
+                    onChange={(e) => setNameOfProduct(e.target.value)}
+                  />
+                </Box>
+                <Box>
+                  <TextField label='descripción'
+                    sx={{ m: 1 }}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </Box>
+                <Box>
+                  <TextField label='Precio' type='number'
+                    sx={{ m: 1 }}
+                    onChange={(e) => setPrice(parseInt(e.target.value))}
 
-      <Box display='flex' justifyContent='center' flexWrap='nowrap' sx={{ m: 3 }}>
-        {
-          products.map(e => (
+                  />
+                </Box>
+                <Box>
+                  <TextField label='Cantidad' type='number'
+                    sx={{ m: 1 }}
+                    onChange={(e) => setQuantity(parseInt(e.target.value))}
 
-            <Box key={e.name} flexWrap='nowrap'>
-              <Chip color='success' label={`${e.name} - ${e.quantity} `}
-                onDelete={() => onDeleteProduct(e.name)}
-                sx={{ m: 1 }}
-              />
+                  />
+                </Box>
+              </Box>
             </Box>
+          </Box>
+        </Box>
+        <Box display='flex' justifyContent='center' sx={{ mt: 4 }}>
+          <Button onClick={() => onCreateProduct()} variant='contained'>
+            agregar producto
+          </Button>
+        </Box>
 
-          ))
-        }
+
+        <Box display='flex' justifyContent='center' flexWrap='nowrap' sx={{ m: 3 }}>
+          {
+            products.map(e => (
+
+              <Box key={e.name} flexWrap='nowrap'>
+                <Chip color='success' label={`${e.name} - ${e.quantity} `}
+                  onDelete={() => onDeleteProduct(e.name)}
+                  sx={{ m: 1 }}
+                />
+              </Box>
+
+            ))
+          }
+        </Box>
+        <Divider sx={{ backgroundColor: 'black', mb: 2 }} />
+        <Box display='flex' justifyContent='center' sx={{ m: 3 }}>
+
+          <InvoicePage order={{
+            name,
+            nOrder,
+            price: total,
+            paymentMethod,
+            products,
+            address,
+
+          }} />
+        </Box>
       </Box>
-      <Divider sx={{ backgroundColor: 'black', mb: 2 }} />
-      <Box display='flex' justifyContent='center' sx={{ m: 3 }}>
-
-        <InvoicePage order={{
-          name,
-          nOrder,
-          price: total,
-          paymentMethod,
-          products,
-          address,
-
-        }} />
-      </Box>
-    </Box>
+    </>
 
   )
 }
